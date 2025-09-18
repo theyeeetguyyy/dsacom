a7ee08c1ea6daf6fd580c67320b7829968179578
anshu <astitvabandil@gmail.com>
2025-09-18 18:08:38 +0530

    login+

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+
 function LoginPage() {
     return (
-        <div>
-            <h1>Login</h1>
-            
+        <>  
+        <div className='parent'>
+            <div className='logindiv'>
+                <h1>Login</h1>
+                <p>Email</p>
+                <input type="text" />
+                <p>Password</p>
+                <input type="text" />
+                <a href="fuckyou">Forgot Password?</a>
+                <button>Submit</button>
+            </div>
         </div>
+        </>
     );
 }
 
